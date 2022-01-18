b0b260b31c0b744cba042b523042369f4b1789e2
Jan Pieczul <jannos1999@gmail.com>
2022-01-18 20:30:12 +0100

    LP-30: Init

---
+++ webapp/src/routesBuilder.ts
@@ -0,0 +1,4 @@
+export default {
+  landingPage: '/',
+  quickBuy: '/quick-buy'
+}
---
+++ webapp/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
