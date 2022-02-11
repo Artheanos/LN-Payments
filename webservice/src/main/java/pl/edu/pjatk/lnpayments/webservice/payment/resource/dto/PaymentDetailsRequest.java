1d8bf446661c3e8f6be00a19be7dac2ab555a73a
oskar117 <olek.konieczny@wp.pl>
2022-02-11 20:45:05 +0100

    LP-60 Try to fix tests

--- webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/dto/PaymentDetailsRequest.java
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/dto/PaymentDetailsRequest.java
@@ -2,6 +2,7 @@ package pl.edu.pjatk.lnpayments.webservice.payment.resource.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.annotation.Nullable;
 import javax.validation.Valid;
@@ -10,6 +11,7 @@ import javax.validation.constraints.Min;
 @Valid
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDetailsRequest {
 
     @Min(1)
