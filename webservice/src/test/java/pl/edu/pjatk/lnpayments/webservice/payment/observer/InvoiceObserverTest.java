610cd50b3264509247aea71d74736f67fbebd18a
oskar117 <olek.konieczny@wp.pl>
2022-02-13 21:28:53 +0100

    LP-67 Fix access midifier

--- webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/observer/InvoiceObserverTest.java
+++ webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/observer/InvoiceObserverTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class InvoiceObserverTest {
+class InvoiceObserverTest {
 
     private ListAppender<ILoggingEvent> logAppender;
 
