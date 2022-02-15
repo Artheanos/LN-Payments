4a83a02ad97c3aab904b7d35cbb39c41724fc25a
Jan Pieczul <jannos1999@gmail.com>
2022-02-15 16:42:57 +0100

    LP-64: Introduce formik

--- webapp/src/components/QuickBuy/Stages/SetupStage.tsx
+++ webapp/src/components/QuickBuy/Stages/SetupStage.tsx
@@ -1,42 +1,43 @@
-import React, { FormEvent } from 'react'
-import { Button, TextField } from '@mui/material'
+import React from 'react'
+import { Button } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { Formik, Field, FormikValues, Form } from 'formik'
 
-import { StageProps } from '../StageProps'
+import { StageProps } from 'components/QuickBuy/StageProps'
+import { TextInput } from 'components/Form/FormikInputs/TextInput'
 
 export const SetupStage: React.FC<StageProps> = ({ onNext }) => {
   const { t } = useTranslation('common')
 
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault()
+  const onSubmit = (form: FormikValues) => {
     onNext()
   }
 
   return (
     <div>
       <div>{t('quickBuy.setup.info')}</div>
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col gap-4 mx-auto mt-12 w-64"
-      >
-        <TextField
-          id="tokenCount"
-          label={t('quickBuy.setup.form.tokenCount.label')}
-          variant="outlined"
-          type="number"
-          defaultValue="1"
-        />
-        <TextField
-          id="email"
-          label={t('quickBuy.setup.form.email.label')}
-          variant="outlined"
-        />
-        <div className="flex justify-end">
-          <Button variant="contained" className="bg-purple-300" type="submit">
-            {t('next')}
-          </Button>
-        </div>
-      </form>
+      <Formik initialValues={{ tokenCount: 1, email: '' }} onSubmit={onSubmit}>
+        <Form className="flex flex-col gap-4 mx-auto mt-12 w-64">
+          <Field
+            name="tokenCount"
+            label={t('quickBuy.setup.form.tokenCount.label')}
+            type="number"
+            variant="outlined"
+            component={TextInput}
+          />
+          <Field
+            name="email"
+            label={t('quickBuy.setup.form.email.label')}
+            variant="outlined"
+            component={TextInput}
+          />
+          <div className="flex justify-end">
+            <Button variant="contained" className="bg-purple-300" type="submit">
+              {t('next')}
+            </Button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   )
 }
