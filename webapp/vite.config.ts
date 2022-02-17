0d0263cf365d288368c95dc4ee42ae9225c2795e
Jan Pieczul <jannos1999@gmail.com>
2022-02-17 23:58:09 +0100

    LP-64: Prefix api endpoints

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
   }
