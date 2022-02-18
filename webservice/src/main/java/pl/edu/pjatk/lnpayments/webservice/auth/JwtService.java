b07dd124201e7380d1bfe29969f77856c5b72f5a
Aleksander Konieczny <olek.konieczny@wp.pl>
2022-02-18 22:15:48 +0100

    LP-70 Create utils for jwt (#24)
    * LP-70 Create utils for jwt
    * LP-70 Fix failing tests
    * LP-70 Fix coverage

---
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/auth/JwtService.java
@@ -0,0 +1,65 @@
+package pl.edu.pjatk.lnpayments.webservice.auth;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Slf4j
+@Service
+public class JwtService {
+
+    private final int expirationInMs;
+    private final String secret;
+
+    public JwtService(
+            @Value("${lnp.auth.expirationInMs}") int expirationInMs,
+            @Value("${lnp.auth.jwtSecret}") String secret) {
+        this.expirationInMs = expirationInMs;
+        this.secret = secret;
+    }
+
+    public String generateToken(String email) {
+        long currentTime = System.currentTimeMillis();
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(currentTime + expirationInMs))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String renewToken(String token) {
+        String email = retrieveEmail(token);
+        return generateToken(email);
+    }
+
+    public String retrieveEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature: {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token: {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token: {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token: {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty: {}", ex.getMessage());
+        }
+        return false;
+    }
+
+}
---
+++ webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/auth/JwtServiceTest.java
@@ -0,0 +1,90 @@
+package pl.edu.pjatk.lnpayments.webservice.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtServiceTest {
+
+    private final String testSecret = "2137";
+    private final int testExpirationTime = 1645124481;
+    private final String testEmail = "test@test.pl";
+    private final JwtService jwtService = new JwtService(testExpirationTime, testSecret);
+    private String testJwt;
+
+    @BeforeEach
+    void setUp() {
+        long testCurrentTime = 1645124481837L;
+        this.testJwt = Jwts.builder()
+                .setSubject(testEmail)
+                .setIssuedAt(new Date(testCurrentTime))
+                .setExpiration(new Date(testCurrentTime + testExpirationTime))
+                .signWith(SignatureAlgorithm.HS512, testSecret)
+                .compact();
+    }
+
+    @Test
+    void shouldGenerateTokenFromEmail() {
+        String token = jwtService.generateToken(testEmail);
+
+        Claims body = Jwts.parser().setSigningKey(testSecret).parseClaimsJws(token).getBody();
+
+        assertThat(body.getSubject()).isEqualTo(testEmail);
+        assertThat(body.getExpiration()).isAfter(body.getIssuedAt());
+    }
+
+    @Test
+    void shouldReturnTrueForValidToken() {
+        boolean result = jwtService.isTokenValid(testJwt);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseForRandomString() {
+        boolean result = jwtService.isTokenValid("fesfsef");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldReturnFalseForEmptyString() {
+        boolean result = jwtService.isTokenValid("");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldReturnFalseForRandomToken() {
+        boolean result = jwtService.isTokenValid("""
+                eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOi
+                IxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0I
+                joxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+                """);
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void shouldRetrieveEmailFromToken() {
+        String email = jwtService.retrieveEmail(testJwt);
+
+        assertThat(email).isEqualTo(testEmail);
+    }
+
+    @Test
+    void shouldGenerateRenewedValidToken() {
+        String newToken = jwtService.renewToken(testJwt);
+        String retrievedEmail = jwtService.retrieveEmail(newToken);
+
+        assertThat(jwtService.isTokenValid(newToken)).isTrue();
+        assertThat(retrievedEmail).isEqualTo(testEmail);
+    }
+
+}
