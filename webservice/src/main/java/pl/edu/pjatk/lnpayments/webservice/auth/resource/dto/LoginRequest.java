6668dda50b1db70caecbe12674a2ff63792868ed
oskar117 <olek.konieczny@wp.pl>
2022-02-23 18:52:44 +0100

    LP-73 Add integration tests for login

--- webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/auth/resource/dto/LoginRequest.java
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/auth/resource/dto/LoginRequest.java
@@ -2,11 +2,13 @@ package pl.edu.pjatk.lnpayments.webservice.auth.resource.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
--- webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/auth/resource/dto/LoginResponse.java
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/auth/resource/dto/LoginResponse.java
@@ -2,9 +2,11 @@ package pl.edu.pjatk.lnpayments.webservice.auth.resource.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import pl.edu.pjatk.lnpayments.webservice.common.entity.Role;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
 
