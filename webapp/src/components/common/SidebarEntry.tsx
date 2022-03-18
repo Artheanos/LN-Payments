1301b9c200de139d4ba6ee387a5599404bc77038
oskar117 <olek.konieczny@wp.pl>
2022-03-18 18:55:12 +0100

    LP-81 Fix eslint import thing

--- webapp/src/components/common/SidebarEntry.tsx
+++ webapp/src/components/common/SidebarEntry.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListItem, ListItemIcon, ListItemText } from '@mui/material'
-import {Link, useLocation} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 type Props = {
   title: string
