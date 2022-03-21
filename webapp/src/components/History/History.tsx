c37a07558fc16c57add42b9fed9b8f1e54b98d37
oskar117 <olek.konieczny@wp.pl>
2022-03-21 20:17:46 +0100

    LP-83 Adjust styles

--- webapp/src/components/History/History.tsx
+++ webapp/src/components/History/History.tsx
@@ -11,7 +11,8 @@ import {
   TableHead,
   TablePagination,
   TableRow,
-  Tooltip
+  Tooltip,
+  Typography
 } from '@mui/material'
 import TablePaginationActions from '@mui/material/TablePagination/TablePaginationActions'
 import VisibilityIcon from '@mui/icons-material/Visibility'
@@ -39,6 +40,27 @@ export const History: React.FC = () => {
       })
   }
 
+  const StatusColor: Record<
+    string,
+    {
+      primary: string
+      secondary: string
+    }
+  > = {
+    PENDING: {
+      primary: '#4fc3f7',
+      secondary: '#29b6f6'
+    },
+    COMPLETE: {
+      primary: '#81c784',
+      secondary: '#66bb6a'
+    },
+    CANCELLED: {
+      primary: '#e57373',
+      secondary: '#f44336'
+    }
+  }
+
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
     newPage: number
@@ -55,7 +77,17 @@ export const History: React.FC = () => {
       queryHistory(0, parseInt(event!.target.value))
     }
   }
+
   if (loading) return <LinearProgress />
+
+  const calculateColor = (paymentStatus: PaymentStatus) => {
+    const colors = StatusColor[paymentStatus as unknown as string]
+    return {
+      backgroundColor: colors.primary,
+      borderColor: colors.secondary
+    }
+  }
+
   return (
     <div className="grow p-14 w-6/12 text-center">
       <TableContainer component={Paper}>
@@ -87,13 +119,50 @@ export const History: React.FC = () => {
                 </TableCell>
                 <TableCell>{payment.price}</TableCell>
                 <TableCell>{payment.numberOfTokens}</TableCell>
-                <TableCell>{payment.paymentStatus}</TableCell>
-                {payment.tokens && (
-                  <TableCell>
-                    <VisibilityIcon />
-                    Show
-                  </TableCell>
-                )}
+                <TableCell>
+                  <Typography
+                    align="center"
+                    sx={{
+                      borderRadius: 8,
+                      fontWeight: 'bold',
+                      display: 'inline-block',
+                      fontSize: '0.75rem',
+                      padding: '3px 10px',
+                      borderStyle: 'solid',
+                      borderWidth: '3px',
+                      width: '100%',
+                      ...calculateColor(payment.paymentStatus)
+                    }}
+                  >
+                    {payment.paymentStatus}
+                  </Typography>
+                </TableCell>
+                <TableCell align="center">
+                  {payment.tokens ? (
+                    <Typography
+                      sx={{
+                        cursor: 'pointer',
+                        fontSize: '0.75rem',
+                        display: 'flex',
+                        alignItems: 'center',
+                        color: '#0d6efd'
+                      }}
+                    >
+                      <VisibilityIcon />
+                      <span className="pl-1">Show</span>
+                    </Typography>
+                  ) : (
+                    <Typography
+                      sx={{
+                        fontStyle: 'italic',
+                        fontSize: '0.85rem',
+                        color: 'darkgray'
+                      }}
+                    >
+                      none
+                    </Typography>
+                  )}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
