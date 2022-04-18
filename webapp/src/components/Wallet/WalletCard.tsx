29cbc08a696f4f3e2e6f6ff136815d1005aa1527
oskar117 <olek.konieczny@wp.pl>
2022-04-18 12:15:56 +0200

    LP-111 Fix prettier

--- webapp/src/components/Wallet/WalletCard.tsx
+++ webapp/src/components/Wallet/WalletCard.tsx
@@ -5,10 +5,7 @@ type Props = {
   standardSize: number
 }
 
-export const WalletCard: React.FC<Props> = ({
-  standardSize,
-  children
-}) => {
+export const WalletCard: React.FC<Props> = ({ standardSize, children }) => {
   return (
     <Grid md={standardSize} xs={12} item>
       <Paper className="flex !flex-col justify-around py-12 px-5 space-y-12">
