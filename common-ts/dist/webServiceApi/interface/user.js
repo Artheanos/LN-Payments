3d0e76d754f364f0ae182db791df120b1aa39998
Jan Pieczul <jannos1999@gmail.com>
2022-05-15 11:55:58 +0200

    Build common-ts in workflows

--- common-ts/dist/webServiceApi/interface/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ROLE_ADMIN";
-    Role["USER"] = "ROLE_USER";
-    Role["TEMPORARY"] = "TEMPORARY_USER";
-})(Role = exports.Role || (exports.Role = {}));
