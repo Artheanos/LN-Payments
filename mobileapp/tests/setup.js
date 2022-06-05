a31fc7c442a4c8d7da830ac8457f8483651ac179
Jan Pieczul <jannos1999@gmail.com>
2022-06-05 19:47:27 +0200

    LP-125: Fixes

--- mobileapp/tests/setup.js
+++ mobileapp/tests/setup.js
@@ -2,15 +2,6 @@
 
 import 'react-native-gesture-handler/jestSetup'
 
-// jest.mock('react-native-reanimated', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const Reanimated = require('react-native-reanimated/mock')
-//
-//   Reanimated.default.call = () => {}
-//
-//   return Reanimated
-// })
-
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 
 // ASYNC STORAGE
