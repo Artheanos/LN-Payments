0692953215d9ea549af16626029cc7af9454d6f9
oskar117 <olek.konieczny@wp.pl>
2022-06-13 18:43:38 +0200

    LP-126 Fix existing tests

--- mobileapp/tests/setup.js
+++ mobileapp/tests/setup.js
@@ -9,3 +9,5 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
+
+jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
