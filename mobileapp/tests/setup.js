2aa0327b5b3ed7461dd56a815529d960d3fc9c40
oskar117 <olek.konieczny@wp.pl>
2022-06-13 18:35:07 +0200

    Revert "LP-126 Progress on tests"
    This reverts commit e510329eedc34a17bb6d40e9b0ea0e19f2f53335.

--- mobileapp/tests/setup.js
+++ mobileapp/tests/setup.js
@@ -9,5 +9,3 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
-
-jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
