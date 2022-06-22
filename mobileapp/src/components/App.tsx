7ccf99c475ee93a3886511c292d63c4b99ddecb6
oskar117 <olek.konieczny@wp.pl>
2022-06-22 21:09:59 +0200

    LP-191 Update default theme to purple

--- mobileapp/src/components/App.tsx
+++ mobileapp/src/components/App.tsx
@@ -1,15 +1,22 @@
 import 'react-native-gesture-handler'
 
 import * as React from 'react'
-import { NativeBaseProvider } from 'native-base'
+import { extendTheme, NativeBaseProvider } from 'native-base'
 import { NavigationContainer } from '@react-navigation/native'
+import colors from 'native-base/src/theme/base/colors'
 
 import { UserContextProvider } from 'components/context/UserContext'
 import { AppRouter } from 'components/routers/AppRouter'
 
+const theme = extendTheme({
+  colors: {
+    primary: colors.purple,
+  },
+})
+
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <UserContextProvider>
         <NavigationContainer>
           <AppRouter />
