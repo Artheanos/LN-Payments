c299b0986bae21bc2b9f70fd078d2cfbb7bada23
oskar117 <olek.konieczny@wp.pl>
2022-07-13 20:45:23 +0200

    LP-196 Enable basic request logging

---
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/common/config/LoggerConfig.java
@@ -0,0 +1,20 @@
+package pl.edu.pjatk.lnpayments.webservice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggerConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLogFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(false);
+        filter.setMaxPayloadLength(64000);
+        return filter;
+    }
+}
