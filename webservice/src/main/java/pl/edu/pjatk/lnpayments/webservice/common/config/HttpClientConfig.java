8635a0a961c82312d9434b3d45f798479eebc077
oskar117 <olek.konieczny@wp.pl>
2022-07-14 19:09:58 +0200

    LP-86 Move resttemplate to separate config class

---
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/common/config/HttpClientConfig.java
@@ -0,0 +1,14 @@
+package pl.edu.pjatk.lnpayments.webservice.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class HttpClientConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/config/WebSocketConfig.java
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/config/WebSocketConfig.java
@@ -1,10 +1,8 @@
 package pl.edu.pjatk.lnpayments.webservice.payment.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -37,10 +35,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
