2c693e04b4d6bcf181390d41da44aadc67483f13
oskar117 <olek.konieczny@wp.pl>
2022-07-16 14:29:40 +0200

    LP-203 Change LND files directory to ~/.lnpayments

--- webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/config/LndConfig.java
+++ webservice/src/main/java/pl/edu/pjatk/lnpayments/webservice/payment/config/LndConfig.java
@@ -12,6 +12,8 @@ import pl.edu.pjatk.lnpayments.webservice.payment.facade.PaymentFacade;
 import javax.net.ssl.SSLException;
 import java.io.File;
 
+import static pl.edu.pjatk.lnpayments.webservice.common.Constants.USER_HOME;
+
 @Configuration
 class LndConfig {
 
@@ -19,28 +21,37 @@ class LndConfig {
     private final int port;
     private final String certificatePath;
     private final String macaroonPath;
+    private final String walletDirectory;
 
     @Autowired
     LndConfig(
             @Value("${lnp.lnd.host}") String host,
             @Value("${lnp.lnd.port}") int port,
             @Value("${lnp.lnd.certificatePath}") String certificatePath,
-            @Value("${lnp.lnd.macaroonPath}") String macaroonPath
+            @Value("${lnp.lnd.macaroonPath}") String macaroonPath,
+            @Value("${lnp.config.workingDirectory}") String walletDirectory
     ) {
         this.host = host;
         this.port = port;
         this.certificatePath = certificatePath;
         this.macaroonPath = macaroonPath;
+        this.walletDirectory = USER_HOME + walletDirectory;
     }
 
     @Bean
     SynchronousLndAPI synchronousLndAPI() throws ClientSideException, SSLException {
-        return new SynchronousLndAPI(host, port, new File(certificatePath), new File(macaroonPath));
+        return new SynchronousLndAPI(host,
+                port,
+                new File(walletDirectory, certificatePath),
+                new File(walletDirectory, macaroonPath));
     }
 
     @Bean
     AsynchronousLndAPI asynchronousLndAPI(PaymentFacade paymentFacade) throws StatusException, SSLException, ValidationException {
-        AsynchronousLndAPI api = new AsynchronousLndAPI(host, port, new File(certificatePath), new File(macaroonPath));
+        AsynchronousLndAPI api = new AsynchronousLndAPI(host,
+                port,
+                new File(walletDirectory, certificatePath),
+                new File(walletDirectory, macaroonPath));
         api.subscribeInvoices(new InvoiceSubscription(), new InvoiceObserver(paymentFacade));
         return api;
     }
