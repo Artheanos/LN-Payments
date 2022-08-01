0d2f508db640896fa1c00ecce42eff66f6a2e1ff
SxLewandowski <42375921+SxLewandowski@users.noreply.github.com>
2022-08-01 20:11:32 +0200

    created Cypress custom commands and replaced with them repeated actions in tests

--- webapp/cypress/support/commands.js
+++ webapp/cypress/support/commands.js
@@ -23,3 +23,42 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps'
+
+Cypress.Commands.add('redirectToPage', () => {
+  Then('I am redirected to {string} Page', (page) => {
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq(page)
+    })
+  })
+})
+
+Cypress.Commands.add('typeData', () => {
+  Given('I enter {string} into {string} field', (data, data_field) => {
+    cy.get('input[name=' + data_field + ']').type(data)
+  })
+})
+
+Cypress.Commands.add('clickButton', () => {
+  When('I click on {string} button', (btn) => {
+    cy.get('button').contains(btn).click()
+  })
+})
+
+Cypress.Commands.add('displayAlert', () => {
+  And('Alert {string} is displayed', (alert) => {
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(alert)
+    })
+  })
+})
+
+Cypress.Commands.add('checkIfAlertExist', () => {
+  Then(
+    'Alert {string} is displayed under {string} field',
+    (alert_under_field) => {
+      cy.contains(alert_under_field).should('exist')
+    }
+  )
+})
