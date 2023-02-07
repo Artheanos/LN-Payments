1d42d3e95ab9d7d4e2c8c509bc42a264cf5b8ec9
oskar117 <olek.konieczny@wp.pl>
2023-02-07 17:23:34 +0100

    LP-212 Remove stupid annotation

--- webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/PaymentResourceIntegrationTest.java
+++ webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/PaymentResourceIntegrationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -35,7 +34,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static pl.edu.pjatk.lnpayments.webservice.helper.factory.UserFactory.createAdminUser;
 import static pl.edu.pjatk.lnpayments.webservice.helper.factory.UserFactory.createStandardUser;
 
-@DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest(classes = IntegrationTestConfiguration.class)
