96a0b9820fc1ba1f2dea79e2191450a5fe5b9798
oskar117 <olek.konieczny@wp.pl>
2023-02-07 17:33:12 +0100

    LP-212 Try to clean DB before tests

--- webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/PaymentResourceIntegrationTest.java
+++ webservice/src/test/java/pl/edu/pjatk/lnpayments/webservice/payment/resource/PaymentResourceIntegrationTest.java
@@ -163,6 +163,7 @@ class PaymentResourceIntegrationTest extends BaseIntegrationTest {
             User admin = userRepository.save(createAdminUser(EMAIL));
             User user = userRepository.save(createStandardUser("admin@admin.pl"));
             String jsonContent = getJsonResponse("integration/payment/response/payments-all-GET-valid.json");
+            paymentRepository.deleteAll();
             paymentRepository.save(new Payment("123", 1, 1, 123, PaymentStatus.PENDING, user));
             paymentRepository.save(new Payment("456", 3, 2, 126, PaymentStatus.COMPLETE, user));
             paymentRepository.save(new Payment("789", 4, 3, 129, PaymentStatus.CANCELLED, user));
@@ -176,6 +177,7 @@ class PaymentResourceIntegrationTest extends BaseIntegrationTest {
 
         @Test
         void shouldReturnOkAndEmptyArrayWhenNoAllPayments() throws Exception {
+            paymentRepository.deleteAll();
             userRepository.save(createAdminUser(EMAIL));
             String jsonContent = getJsonResponse("integration/payment/response/payments-GET-empty.json");
 
